1edb9b6bdb051573885c73551f5e4637ef7d0839
Ali Karababa <karababa.alii@gmail.com>
2021-04-01 13:46:56 +0300

    Initial Commit

---
+++ Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Ali Karababa on 1.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
